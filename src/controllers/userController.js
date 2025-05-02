b49b4457215d4b06872c1f2dce146661f4eaaa03
Clinx Daily <clinxdaily@gmail.com>
2025-05-02 09:43:45 +0700

    #39

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ let handleLogin = async(req,res)=>{
     });
 }
 let handleGetAllUsers = async(req,res)=>{
-    let id = req.body.id; //lấy dữ liệu từ client gửi lên
+    let id = req.query.id; //lấy dữ liệu từ client gửi lên
     if(!id){
         return res.status(200).json({
             errCode: 1, //mã lỗi
@@ -34,7 +34,29 @@ let handleGetAllUsers = async(req,res)=>{
         users //trả về danh sách người dùng
     })
 }
+let handleCreateNewUser = async (req,res)=>{
+    let message = await userService.createNewUser(req.body);
+    return res.status(200).json(message);
+}
+let handleDeleteUser = async (req, res) =>{
+    if(!req.body.id){
+        return res.status(200).json({
+            errCode: 1,
+            message: 'Missing required parameters'
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message);
+}
+let handleEditUser = async(req, res) =>{
+    let data = req.body;
+    let message = await userService.updateUserData(data);
+    return res.status(200).json(message);
+}
 module.exports = {
     handleLogin: handleLogin,
-    handleGetAllUsers:handleGetAllUsers
+    handleGetAllUsers:handleGetAllUsers,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser:handleEditUser,
+    handleDeleteUser:handleDeleteUser
 }
